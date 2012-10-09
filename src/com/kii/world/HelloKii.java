51141350abe73aa384634847b42a6b739675aac0
Chris Beauchamp <cjbeauchamp@gmail.com>
2012-10-08 21:12:06 -0700

    updated site variable

--- src/com/kii/world/HelloKii.java
+++ src/com/kii/world/HelloKii.java
@@ -11,7 +11,7 @@ public class HelloKii extends Application {
         super.onCreate();
                 
         // initialize the Kii SDK!
-        Kii.initialize("__KII_APP_ID__", "__KII_APP_KEY__", "__KII_API_URL__");
+        Kii.initialize("__KII_APP_ID__", "__KII_APP_KEY__", __KII_APP_SITE__);
     }
 	
 }
